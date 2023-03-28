50a0e00766af2436ea9d6eb9cfa3314366d91a4f
Tibi0821 <telekitibi21@gmail.com>
2023-03-28 13:17:06 +0300

    aranjat fisiere pentru lab3 si lab 4, pregatire ptr lab5

---
+++ tsc-lab4/lab_dut/instr_register_pkg.sv
@@ -0,0 +1,27 @@
+package instr_register_pkg;
+  timeunit 1ns/1ns;
+
+  typedef enum logic [3:0] { 
+  	ZERO,
+    PASSA,
+    PASSB,
+    ADD,
+    SUB,
+    MULT,
+    DIV,
+    MOD
+  } opcode_t;
+
+  typedef logic signed [31:0] operand_t;
+  typedef logic signed [63:0] operand_rez;
+  typedef logic [4:0] address_t;
+  
+  typedef struct {
+    opcode_t  opc;
+    operand_t op_a;
+    operand_t op_b;
+    operand_rez result;
+   
+  } instruction_t;
+
+endpackage: instr_register_pkg
